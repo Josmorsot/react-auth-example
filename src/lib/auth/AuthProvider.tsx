4576f5a17700a742eb73f87c5c43826a3e36d571
Ivan <imoreno@carto.com>
2024-09-28 15:54:14 +0200

    implement authentication workflows

--- src/lib/auth/AuthProvider.tsx
+++ src/lib/auth/AuthProvider.tsx
@@ -1,19 +1,34 @@
-import { ReactNode } from 'react'
-import { AuthInitializeConfig } from './types'
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { Auth, AuthInitializeConfig } from "./types";
 
 interface AuthProviderProps extends AuthInitializeConfig {
-  children?: ReactNode
+  children?: ReactNode;
 
   /**
    * @see {@link AuthInitializeConfig.initialTokens}
    */
-  initialTokens?: AuthInitializeConfig['initialTokens']
+  initialTokens?: AuthInitializeConfig["initialTokens"];
 
   /**
    * @see {@link AuthInitializeConfig.onAuthChange}
    */
-  onAuthChange?: AuthInitializeConfig['onAuthChange']
+  onAuthChange?: AuthInitializeConfig["onAuthChange"];
 }
+type UnwrapPromise<P> = P extends Promise<infer R> ? R : P;
+type TokenSet = UnwrapPromise<AuthProviderProps["initialTokens"]>
+type UserData = Auth["currentUser"]
+interface AuthContextProps {
+  tokens?: TokenSet
+  user?: UserData
+  onAuthChange: ({ tokens, user}: { tokens:  NonNullable<TokenSet> | null, user: UserData }) => void
+}
+
+
+const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
+
+const useAuthContext = () => {
+  return useContext(AuthContext);
+};
 
 /**
  * Initializes the auth state and exposes it to the component-tree below.
@@ -22,11 +37,38 @@ interface AuthProviderProps extends AuthInitializeConfig {
  * a single source of truth.
  */
 function AuthProvider(props: AuthProviderProps): JSX.Element {
-  const { initialTokens, onAuthChange, children } = props
+  const { initialTokens, onAuthChange: onTokensChange, ...otherProps } = props;
+  const [tokens, setTokens] = useState<UnwrapPromise<typeof initialTokens>>();
+  const [user, setUser] = useState<UserData>()
 
-  return (
-    <>{children}</>
-  )
+  const onAuthChange: AuthContextProps["onAuthChange"] = ({ tokens, user }) => {
+    setTokens(tokens)
+    setUser(user)
+    onTokensChange?.(tokens);
+  };
+
+  useEffect(() => {
+    const getInitialTokens = async () => {
+      const tokens = await initialTokens;
+      setTokens(tokens);
+      
+      if (!tokens) {
+        setUser(null)
+      }
+    };
+
+    getInitialTokens().catch(() => {
+      console.error("An error has occurred");
+    });
+  }, [initialTokens]);
+
+  const value = {
+    tokens,
+    user,
+    onAuthChange,
+  };
+
+  return <AuthContext.Provider value={value} {...otherProps} />;
 }
 
-export { AuthProvider, type AuthProviderProps }
+export { AuthProvider, type AuthProviderProps, useAuthContext };
--- src/lib/auth/useAuth.ts
+++ src/lib/auth/useAuth.ts
@@ -1,4 +1,8 @@
-import { Auth } from './types'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Auth } from "./types";
+import { useAuthContext } from "./AuthProvider";
+import { useApiFetcher } from "../api";
+import { useCallback, useEffect } from "react";
 
 /**
  * Returns the current auth state. See {@link Auth} for more information on
@@ -7,17 +11,70 @@ import { Auth } from './types'
  * @throws {TypeError} if called from a component not descendant of AuthProvider
  */
 function useAuth(): Auth {
+  const fetcher = useApiFetcher();
+  const { tokens, user, onAuthChange } = useAuthContext();
+
+  const getUser = useCallback(async (accessToken?: string) => {
+    const userResponse = await fetcher(
+      "GET /v1/users/me",
+      {},
+      accessToken ? { headers: { authorization: `Bearer ${accessToken}` } } : undefined
+    );
+    if (!userResponse.ok) {
+      throw new Error(userResponse.data.message)
+    }
+
+    return {
+      name: userResponse.data.displayName,
+      email: userResponse.data.email ?? "-",
+      userId: userResponse.data.userId,
+    };
+  }, [fetcher]);
+
+  const getTokenWithCredentials = async (credentials: { email: string, password: string}) => {
+    const tokensResponse = await fetcher("POST /v3/auth/login", { data: credentials });
+    if (!tokensResponse.ok) {
+      throw new Error(tokensResponse.data.message);
+    }
+
+    return {
+      access: tokensResponse.data.accessToken,
+      refresh: tokensResponse.data.refreshToken,
+      accessExpiresAt: tokensResponse.data.accessTokenExpiresAt,
+      refreshExpiresAt: tokensResponse.data.refreshTokenExpiresAt,
+    };
+  }
+
+  useEffect(() => {
+    if (tokens && !user) {
+      getUser(tokens.access).then((user) => { // TODO: Re-check why `defaultHeaders` is not working properly
+        onAuthChange({ tokens, user })
+      }).catch((reason) => {
+        // TODO
+        console.error(reason)
+      })
+    }
+  }, [getUser, tokens, user, onAuthChange]);
+
   return {
-    tokens: null,
-    currentUser: null,
-    login(credentials) {
-      const { email, password } = credentials
-      return Promise.reject(new Error('Not yet implemented'))
+    tokens,
+    currentUser: user,
+    async login(credentials) {
+      try {
+        const tokenSet = await getTokenWithCredentials(credentials)
+        const user = await getUser(tokenSet.access)
+        
+        onAuthChange({ tokens: tokenSet, user });
+      } catch (error) {
+        return Promise.reject(error as Error)
+      }
+
     },
     logout() {
-      return Promise.reject(new Error('Not yet implemented'))
+      onAuthChange({ tokens: null, user: null });
+      return Promise.resolve()
     },
-  }
+  };
 }
 
-export { useAuth }
+export { useAuth };
